89f36f264871404a22be553030eaaa378c4767fd
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-11-15 00:30:41 -0500

    Completes intial setup

--- server/server.js
+++ server/server.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('../client/dist'));
+app.use(express.static('client/dist'));
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
