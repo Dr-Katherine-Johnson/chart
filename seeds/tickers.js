aba86c444e3a41e8e86f6f0aa433e7324e5b73e7
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-11-15 04:27:35 -0500

    Finishes basic ticker seeding function

--- seeds/tickers.js
+++ seeds/tickers.js
@@ -1,25 +1,14 @@
-
-
-
-
-
-
-
-
 // TODO: add test suite
 
-
 function createTickers() {
-  // should return an array of 100 strings (tickers)
-    // each ticker is 4 characters in length // TODO: vary the ticker length
+  // returns the same array of 100 unique strings (tickers)
+    // each ticker is 3 - 4 characters in length // TODO: vary the ticker length
     // each ticker is made up of the letters A - Z
-      // unicode code's between 0041 and 005A (in hexadecimal notation)
-    // the seed argument is the number of sequential unicode codes to skip for the next digit in the ticker (try seed values between 1 - 5)
-    // the next ticker in the array uses a seed 1 greater than the previous seed ??
 
     let seed = 1;
     let result = [];
     let ticker = [];
+
     // start at decimal 65
     let startValue = 65;
     let decimal = 65;
@@ -28,7 +17,7 @@ function createTickers() {
 
     for (let i = 0; i < 100; i++) {
       for (let j = 0; j < 4; j++) {
-        if (decimal < 65 || decimal > 90) {
+        if (decimal + j < 65 || decimal + j > 90) {
           incrementor++;
           seed = 1;
           decimal = startValue;
@@ -42,11 +31,8 @@ function createTickers() {
 
 
         // every time the incrementor changes, build the ticker in a different order
-        // console.log('incrementor: ', incrementor);
-        // console.log('decimal: ', decimal);
-
         if (incrementor === 0) {
-            ticker.push(letter);
+          ticker.push(letter);
         } else if (incrementor === 1) {
           ticker.unshift(letter);
         } else if (incrementor === 2) {
@@ -67,10 +53,6 @@ function createTickers() {
 
       result.push(ticker.join(''));
       ticker = [];
-
-      // if (i !== 0 && i % 51 === 0) {
-      //   seed++;
-      // }
     }
     return result;
 }
@@ -78,9 +60,6 @@ function createTickers() {
 let tickers = createTickers();
 console.log('tickers: ', tickers)
 
+// to verify that each ticker in the array is in fact unique
 let mySet = new Set(tickers);
 console.log(mySet);
-
-function makeTicker() {
-  // needs to have several different permutations of filling up a 4 character string, depending on it's input
-}
