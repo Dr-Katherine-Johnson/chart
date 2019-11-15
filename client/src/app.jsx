0b3bc19edf8435b2951c89d2eedd4e693e0706be
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-11-14 23:02:18 -0500

    Initial setup

---
+++ client/src/app.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Hello!</div>
+    );
+  }
+}
+
+ReactDOM.render(<App></App>, document.querySelector('#root'));
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/src/app.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'client/dist')
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  }
+}
