e9da89e621fa7e25d8a519f5d9f5a31066b65b07
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-11-16 18:12:45 -0500

    [wip] Adds placeholder chart

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Sample from '../../sampledata/price.js'
+
+import ChartHat from './charthat.jsx';
+
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      ticker: Sample.ticker,
+      name: Sample.name,
+      prices: Sample.prices
+    }
   }
 
   render() {
     return (
-      <div>Hello!</div>
+      <div className="priceChart">
+        <ChartHat
+          ticker={this.state.ticker}
+        >
+        </ChartHat>
+        <div className="rating-percent"></div>
+        <div className="people-own"></div>
+      </div>
     );
   }
 }
---
+++ client/src/charthat.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ChartHat = ({ ticker, children }) => {
+  return (
+    <>
+      <h1>{ticker}</h1>
+      <div>This is the price that is currently being hovered over</div>
+      {/* TODO: what are these percent changes computed from?? */}
+      <div>Absolute and % change</div>
+
+    </>
+  );
+};
+
+export default ChartHat;
