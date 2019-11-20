9b6788890c18d66cc831e974e992e8c52db03e21
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-11-19 23:14:25 -0500

    Adds company name generator and unit test

--- seeds/prices.js
+++ seeds/prices.js
@@ -1,4 +1,33 @@
+const request = require('request');
+const faker = require('faker');
+
+
 // needs to import the array of all the tickers
   // iterate through the array
     // generate random, unique values for each relevant property
+    // {
+    //   ticker: STRING,
+    //   name: STRING,
+    //   prices: [
+    //     {
+    //       dateTime: DATE,
+    //       open: NUMBER,
+    //       high: NUMBER,
+    //       low: NUMBER,
+    //       close: NUMBER,
+    //       volume: INTEGER
+    //     },
+    //     // ...
+    //   ]
+    // }
     // creating a document in Mongoose with that ticker
+module.exports = {
+  generateName() {
+    // request('https://uinames.com/api/', (err, response, body) => {
+    //   if (err) { return console.log(err); }
+    //   return body;
+    // });
+
+    return faker.company.companyName();
+  }
+}
--- tests/seedingtests.js
+++ tests/seedingtests.js
@@ -2,24 +2,46 @@ const mocha = require('mocha');
 const expect = require('chai').expect;
 
 const tickers = require('../seeds/tickers.js');
-describe('Tickers', () => {
+const prices = require('../seeds/prices.js');
+
+// TICKER SEEDING SCRIPT
+describe('Tickers Seeding Script', () => {
   const tickerList1 = tickers.createTickers();
   const tickerList2 = tickers.createTickers();
-  describe('Seed', () => {
-    it('Should be a string', () => {
-      tickerList1.forEach(ticker => {
-        expect(ticker).to.be.a('string');
-      })
+
+  it('Should generate an array of strings', () => {
+    tickerList1.forEach(ticker => {
+      expect(ticker).to.be.a('string');
     })
-    it('Should generate 100 tickers', () => {
-      expect(tickerList1).to.have.lengthOf(100);
+  })
+  it('Should generate 100 tickers', () => {
+    expect(tickerList1).to.have.lengthOf(100);
+  });
+  it('On repeated calls, should generate the same 100 tickers, in the same order', () => {
+    expect(JSON.stringify(tickerList1)).to.equal(JSON.stringify(tickerList2));
+  });
+  it('Each ticker should be different', () => {
+    const tickerSet = new Set(tickerList1);
+    expect(tickerSet.size).to.equal(100);
+  });
+});
+
+// PRICES SEEDING SCRIPT
+describe('Prices Seeding Script', () => {
+  describe('Name', () => {
+    const name1 = prices.generateName();
+    const name2 = prices.generateName();
+
+    it('Should generate string', () => {
+      expect(name1).to.be.a('string');
     });
-    it('On repeated calls, should generate the same 100 tickers, in the same order', () => {
-      expect(JSON.stringify(tickerList1)).to.equal(JSON.stringify(tickerList2));
-    });
-    it('Each ticker should be different', () => {
-      const tickerSet = new Set(tickerList1);
-      expect(tickerSet.size).to.equal(100);
+
+    it('Should generate a random name', () => {
+      expect(name1).to.not.equal(name2);
     });
   });
+
+  xdescribe('Date', () => {
+    it('Should generate a random Date')
+  })
 });
