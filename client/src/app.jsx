8921d89c319e3506dc6c80beb5bb0a01ccadb70c
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-11-22 22:23:10 -0500

    Loads the price data into state

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import Sample from '../../sampledata/price.js'
-
+import $ from 'jquery';
 import ChartHat from './charthat.jsx';
 import Chart from './chart.jsx';
 
@@ -17,6 +16,30 @@ class App extends React.Component {
       ratingPercent: `81%`,
       peopleOwn: 2500
     }
+  }
+
+  componentDidMount() {
+    // ajax request to the server for price data
+    $.ajax({
+      // TODO: fixed stock ticker for now
+      url: `http://localhost:3000/price/${'ABCD'}`,
+      dataType: 'json',
+      success: (ticker) => {
+        ticker.prices.forEach(price => {
+          // convert the ISO8601 string into a Date object in the local timezone
+          price.dateTime = new Date(new Date(price.dateTime).getTime() + new Date().getTimezoneOffset() * 60 * 1000);
+        });
+
+        this.setState({
+          ticker: ticker.ticker,
+          name: ticker.name,
+          prices: ticker.prices
+        });
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    });
   }
 
   render() {
