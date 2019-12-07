d04ddbd3cf65bbc09f877460cddf0eb3c48f9514
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-12-07 16:33:51 -0500

    Serves static files from /public and /dist directories

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const PORT = 3000;
 const controller = require('../controller/index.js');
 
 app.use(express.static('client/dist'));
+app.use(express.static('client/public'));
 
 app.get('/price/:ticker', controller.getTicker);
 
