677ed664a47180674c115dc9d4ab644bcad715f3
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-12-08 16:11:42 -0500

    Adds test for verticalline component

--- tests/chart.test.js
+++ tests/chart.test.js
@@ -4,14 +4,8 @@ import { shallow } from 'enzyme';
 import Chart from '../client/src/chart.jsx';
 
 describe('Chart', () => {
-  const wrapper = shallow(<Chart offsetX={100} offsetY={50}/>);
+  const wrapper = shallow(<Chart />);
   it('Contains an <svg>', () => {
     expect(wrapper.find('svg')).toHaveLength(1);
-  });
-
-  it('Displays a vertical line when offsetX and offsetY are NOT null', ()=> {
-    console.log(wrapper.props());
-    // console.log(wrapper.props().offsetX);
-    // console.log(wrapper.props().children);
   });
 });
