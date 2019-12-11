e713a5aea70e1c391361a47be3e197eecbf6a57c
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-12-10 22:23:00 -0500

    Changes visibility:hidden to use a class for ease of testing

--- client/src/verticalline.jsx
+++ client/src/verticalline.jsx
@@ -2,15 +2,19 @@ import React from 'react';
 
 const VerticalLine = ({ offsetX, offsetY, activeDateTime }) => {
   // when offsetX OR offsetY is null, hide the whole VerticalLineContainer with CSS
-  let style = {
-    visibility: 'hidden'
-  }
+  let style = {}
+  let visible = 'hidden';
   if (offsetX !== null && offsetY !== null) {
-    style = Object.assign({}, { style: 'visible', left: offsetX });
+    style = Object.assign({}, { left: offsetX });
+    visible = null;
   }
 
   const verticalLineContainer = (
-    <div id="chart-vertical-line-container" style={style}>
+    <div
+      id="chart-vertical-line-container"
+      style={style}
+      className={visible}
+    >
       <div id="chart-datetime-container">
         {activeDateTime ? activeDateTime.toDateString() : null}
       </div>
