f1abed003a7960438161eed41dd106f75fcb8666
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-12-30 15:42:18 -0500

    Runs seed script before starting server in npm command docker-start

--- start.sh
+++ start.sh
@@ -1,5 +1,2 @@
 # Execute this file once once docker is installed
 docker-compose up -d
-# TODO: workaround because dockerthe npm run start-docker doesn't seem to be running the seeding script ...
-# docker exec ${MACHINE_NAME}_chart_1 node seeds/start.js
-docker exec -d chart_chart_1 node seeds/start.js
