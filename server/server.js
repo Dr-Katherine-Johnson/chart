fe92f91274535d8c3fc922c89dc4cb10cee32843
Manuela Gomez <mmanu.gomez@gmail.com>
2020-01-21 07:49:54 -0500

    [WIP] Add post route for price and body parser

--- server/server.js
+++ server/server.js
@@ -4,13 +4,19 @@ const config = require('../env.config.js');
 const PORT = config.SERVICE_CHART_PORT;
 const controller = require('../controller/index.js');
 const cors = require('cors');
+const bodyParser = require('body-parser')
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('client/dist'));
 app.use(express.static('client/public'));
 
 app.get('/price/:ticker', controller.getTicker);
 app.get('/current-price/:ticker', controller.getCurrentPrice);
 app.get('/percent-change/:ticker', controller.getPercentChange);
+
+// Create a new item
+app.post('/price/:ticker', controller.addTicker);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
