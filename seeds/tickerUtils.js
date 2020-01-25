5cd470f44f87b6655a8275c3071165b75c0a59f7
Manuela Gomez <mmanu.gomez@gmail.com>
2020-01-25 16:03:21 -0500

    [chooseKCombos] generates all unique k length combos for string

--- seeds/tickerUtils.js
+++ seeds/tickerUtils.js
@@ -1,7 +1,8 @@
 const _ = require('lodash');
 
 module.exports = {
-  // We'll generate charCodes in order of Uppercase letters, Lowercase Letters, and then Numbers 0-9
+  // We'll generate characters in order of Uppercase letters, Lowercase Letters, and then Numbers 0-9
+  // TODO: would be nice to generate only upper alpha, upper+lower, upper+lower+nums, depending on what you needed
   getAlphaNumeric () {
     let alphaNums = [];
     // [65 - 90]  upper alpha (A-Z)
@@ -20,5 +21,40 @@ module.exports = {
       alphaNums.push(char);
     }
     return alphaNums;
+  },
+  // return an array of all unique combinations of length K each for a set of M elements -> M choose K
+        // QUESTION: would it be better to deal with strings or arrays as the first arg?
+  chooseKCombos (string, k) {
+    // initialize combination array
+    var combos = [];
+    // Edge cases: make sure k >= the number of elements in set
+    if (k > string.length) {
+      // no solution
+      return new Error(`Not enough elements to generate combinations of length ${k}`);
+    } else if (k === string.length) {
+    // edge case: set.length choose k < maxCombos?
+    // two easy cases
+    // 1. you're choosing M out of M elements
+      return [string]; // only one possible combo
+    } else if (k === 1) {
+      // if you're choosing 1, there are M max combinations
+      for (let i = 0; i < string.length; i++) {
+        combos.push(string[i])
+      }
+      return combos;
+    }
+    // otherwise compute combinations
+    // When the length drops below (k-1), we cannot find any (k-1)-combos, hence the upper limit for the iteration:
+    for (let i = 0; i < string.length; i++) {
+      var headChar = string.substring(i, i + 1);
+      // Find the combinations for the tail/end of the string
+      var tailCombos = this.chooseKCombos(string.slice(i + 1), k - 1);
+      // For each (k-1)-combination we join it with the current
+      // and store it to the set of k-combinations.
+      for (let j = 0; j < tailCombos.length; j++) {
+        combos.push(headChar.concat(tailCombos[j]));
+      }
+    }
+    return combos;
   }
 };
