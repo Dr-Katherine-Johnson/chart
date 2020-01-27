447191d8c57d8705385dc0d2f7a98e88b687f9df
Manuela Gomez <mmanu.gomez@gmail.com>
2020-01-27 07:27:21 -0500

    [WIP] Using require statements instead of ES6

--- seeds/influx/index.js
+++ seeds/influx/index.js
@@ -1,15 +1,16 @@
-import Client from '@influxdata/influx';
-import data from './seed.js';
+const Client = require('@influxdata/influx');
+const data = require('./seed.js');
 
 // how to update the environment variables for different databases?
 const token = process.env.INFLUXDB_TOKEN
 const client = new Client('http://127.0.0.1:9999/api/v2', token)
 
 // here is where we have data in Line protocol string
-// QUESTION: Would you still have to batch the data?
-const data = ''
+const measurement = 'prices';
+const data = data.getLineProtocolString(measurement,100);
 const bucket = 'robinhood-chart'
 // should the orgID go into .env?
 const orgID = '66c5cd13f69410bd'
 
+// QUESTION: Would you still have to batch the data?
 client.write.create(orgID, bucket, data)
