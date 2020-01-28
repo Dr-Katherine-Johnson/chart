31eb51cc6657460de670eab9dad00a1f2fd5ac23
Manuela Gomez <mmanu.gomez@gmail.com>
2020-01-27 22:05:36 -0500

    [WIP] Added comments and ref to official documentation

--- seeds/influx/index.js
+++ seeds/influx/index.js
@@ -7,10 +7,13 @@ const client = new Client('http://127.0.0.1:9999/api/v2', token)
 
 // here is where we have data in Line protocol string
 const measurement = 'prices';
+// here we are hard-coding the data?
+const records = 100;
 const data = data.getLineProtocolString(measurement,100);
 const bucket = 'robinhood-chart'
 // should the orgID go into .env?
 const orgID = '66c5cd13f69410bd'
 
-// QUESTION: Would you still have to batch the data?
+// QUESTION: Would you still have to batch the data? -> see Docs:
+// https://v2.docs.influxdata.com/v2.0/write-data/best-practices/optimize-writes/#batch-writes
 client.write.create(orgID, bucket, data)
