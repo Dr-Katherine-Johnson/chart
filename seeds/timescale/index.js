e3e8ea3a1b37d0d04107e4bab8b7580ff635a851
Manuela Gomez <mmanu.gomez@gmail.com>
2020-02-13 01:39:48 -0500

    [WIP] timescale index file to seed database

--- seeds/timescale/index.js
+++ seeds/timescale/index.js
@@ -1,9 +1,10 @@
-// TODO test bash script and run using child_process module
-const { exec } = require('child_process');
-var script = exec('sh seed.sh', (err, stdout, stderr) => {
-  if (err) {
-    console.log(`exec error: ${err}`);
-  }
-  console.log(stdout);
-  console.log(stderr);
-})
+const { seedTSDB } = require('./timescale-seed.js');
+const { dockerTSDBLoadSchema } = require('./seedUtils.js');
+
+// After starting up the container
+// Create database
+// load the schema
+dockerTSDBLoadSchema('chart.sql');
+// seed database
+seedTSDB(200);
+
