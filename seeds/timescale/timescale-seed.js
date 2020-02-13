b48934fa475795a1469b685a3d97d4d01cda9acf
Manuela Gomez <mmanu.gomez@gmail.com>
2020-02-13 01:40:51 -0500

    [Timescale seeding function] refactor to use utils to write csv, connect to DB, load data to DB

--- seeds/timescale/timescale-seed.js
+++ seeds/timescale/timescale-seed.js
@@ -3,82 +3,65 @@
 const prices = require('../prices.js');
 const tickers = require('../tickers.js');
 
-/////////////////////////////  WRITING TO CSV SETUP /////////////////////////////////////////
+/////////////////////////  WRITING TO CSV/DATABSE SETUP //////////////////////////////////
 const fs = require('fs');
-// open the file we want to write our tickers and prices to
-const wsTickers = fs.createWriteStream('./seeds/timescale/tickers.csv');
-const wsPrices = fs.createWriteStream('./seeds/timescale/prices.csv');
-/////////////////////////////////////////////////////////////////////////////////////////////
+const path = require('path');
+const { write, deleteFile, dockerCopy } = require('./seedUtils.js');
+const { timescale, loadDataToTable } = require('./timescale-client.js')
+//////////////////////////////////////////////////////////////////////////////////////////
 
-//////////////////////////// LOADING TO DATABASE ////////////////////////////////////////////
-import path from 'path';
-// the child_process allows us to execute commands
-const { spawn } = require('child_process');
-/////////////////////////////////////////////////////////////////////////////////////////////
 
-////////////////////////// FUNCTION THAT WRITES TO THE TWO CSV FILES ////////////////////////
-const writeTickerData = async (maxTickers, tickerWriter, pricesWriter) => {
-  console.time("all");
-  // get the unique tickers
-  const allTickers = tickers.createNTickers(maxTickers);
-  // since we're using a while loop initialize our index
-  let currentTickerIdx = 0;
-  // first csv file is the outer loop and write the csv for our tickers table
-  while (currentTickerIdx < maxTickers) {
-    let currentTicker = allTickers[currentTickerIdx];
-    // generate the price list for each ticker
-    let priceList = prices.generatePricesList();
-    // initialize the index to iterate over the prices
-    let currentPriceIdx = 0;
-    // second csv file writes the csv for our prices table
-    while (currentPriceIdx < priceList.length) {
-      // each price for a stock or ticker is an object with a series of properties
-      const priceObject = priceList[currentPriceIdx];
-      const { dateTime, open, high, low, close, volume }  = priceObject;
-      // get the line for each priceObject to write to the csv
-      const priceData = `${currentTicker},${dateTime},${open},${high},${low},${close},${volume}\n`;
-      await write(pricesWriter, priceData);
-      currentPriceIdx++
-    }
-    var company = prices.generateName();
-    var tickerData = `${currentTicker},"${company}"\n`;
-    await write(tickerWriter, tickerData);
-    currentTickerIdx++;
-    // NOTE check how long it takes to load up that 20MB file - a couple of seconds
-    if ((currentTickerIdx + 1) % 200 === 0) {
-      // end the writestreams
-      wsTickers.end();
-      wsPrices.end();
-      // run a bash commands with child process
-        // copies the csv file into the container
+////////////////////////// FUNCTION THAT WRITES TO THE TWO CSV FILES /////////////////////
+module.exports = {
+  writeTickerDataToCSV: async function (maxTickers) {
+    // open the files we want to write our tickers and prices to
+    let tickerFile = path.join(__dirname, `tickers.csv`);
+    let pricesFile = path.join(__dirname, `prices.csv`);
+    let tickerWriter = fs.createWriteStream(tickerFile);
+    let pricesWriter = fs.createWriteStream(pricesFile);
+    // start a timer
+    console.time("all");
+    // get the unique tickers
+    const allTickers = tickers.createNTickers(maxTickers);
+    // since we're using a while loop initialize our index
+    let currentTickerIdx = 0;
+    // first csv file is the outer loop - tickers
+    while (currentTickerIdx < maxTickers) {
+      let currentTicker = allTickers[currentTickerIdx];
+      // generate the price list for each ticker
+      let priceList = prices.generatePricesList();
+      // initialize the index to iterate over the prices
+      let currentPriceIdx = 0;
+      // second csv file writes the csv for our prices table
+      while (currentPriceIdx < priceList.length) {
+        // each price element for a stock or ticker is an object with a series of properties
+        const priceObject = priceList[currentPriceIdx];
+        const { dateTime, open, high, low, close, volume }  = priceObject;
+        // get the line for each priceObject to write to the csv
+        const priceData = `${currentTicker},${dateTime},${open},${high},${low},${close},${volume}\n`;
+        // write to csv and wait for result before moving onto next priceObject
+        await write(pricesWriter, priceData);
+        currentPriceIdx++
+      }
+      // Once we finished writing all the prices for the ticker write the ticker's data
+      var company = prices.generateName();
+      // the name might have spaces and commas so use "Name, of Company" to load into postgres
+      var tickerData = `${currentTicker},"${company}"\n`;
+      await write(tickerWriter, tickerData);
+      currentTickerIdx++;
+      // Every 200 tickers + corresponding prices ~ 20MB csv load to database
+      if ((currentTickerIdx + 1) % 200 === 0) {
         // executes the COPY command using postgres
-        // deletes the file
-      // recreate the stream
+        await loadDataToTable(tickerFile, 'tickers');
+        await loadDataToTable(pricesFile, 'prices');
+        // deletes the files so we make better use of space
+        await deleteFile(tickerFile);
+        await deleteFile(pricesFile);
+        // recreate the streams
+        tickerWriter = fs.createWriteStream(path.join(__dirname, `tickers${currentFile}.csv`));
+        pricesWriter = fs.createWriteStream(path.join(__dirname, `prices${currentFile}.csv`));
+      }
     }
-  }
-  console.timeEnd("all");
+    console.timeEnd("all");
+  },
 }
-
-// prefents backpressuring when writing to stream
-// returns a promise when we get a drain, only in that case does await need to be called
-// this is because there are a max number of bytes that can be stored inside a writeable stream's internal buffer
-// see nodejs.org/api/stream.html#stream_even_drain
-const write = (writer, data) => {
-  return new Promise((resolve) => {
-    if (!writer.write(data)) {
-      writer.once('drain', resolve)
-    } else {
-      resolve()
-    }
-  })
-}
-
-// copies the csv file into the container
-const copyToDocker = (data, table) => {
-  const dockerCP = spawn('docker', ['cp', path.join(__dirname, `${table}.csv`), `timescaledb:/${table}.csv`]);
-  return new Promise((resolve) => {
-    dockerCP.stdout.on('done', resolve});
-  })
-}
-
-writeTickerData(200, wsTickers, wsPrices)
