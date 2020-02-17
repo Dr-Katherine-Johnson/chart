529608e12fb7be2cf3fbc2ba08db0b83bd46dd8f
Manuela Gomez <mmanu.gomez@gmail.com>
2020-02-16 23:20:54 -0500

    Fixed calculations for skipping weekends and refactored conditionals

--- seeds/prices.js
+++ seeds/prices.js
@@ -75,30 +75,29 @@ module.exports = {
     const msInHour = 3600000;
     // index is 0, generate 9:30 AM on today's date
     if (index === 0) {
-      const now = new Date();
-      dateTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 9 - (new Date().getTimezoneOffset() / 60), 30);
+      const start = new Date('January 1, 2019 23:15:30');
+      dateTime = new Date(start.getFullYear(), start.getMonth(), start.getDate(), 9 - (new Date().getTimezoneOffset() / 60), 30);
 
-    } else if (index % 15 === 0) {
-      // skip weekends, so that every 5 days, with three points per day, skip to monday
-      var timeToNextMonday = 17 * 48 * msInHour;
+    } else if (prev) {
       prevDateTime = new Date(prev.dateTime);
-      dateTime = new Date(prevDateTime.getTime() + timeToNextMonday);
-    } else if (index % 3 === 0) {
-      // index DOES divide by 7 evenly, add the amount of milliseconds to the next day's 9:30 AM
-      // CHANGE: checking how 1 day looks being rendered by 3 points since with too
-      // many points per day they don't fit in the EC2 instance
-      // from 4:30 pm to 9:30 am, there are 17 hours
-      prevDateTime = new Date(prev.dateTime);
-      var timeToNextDay = 17 * msInHour;
-      dateTime = new Date(prevDateTime.getTime() + timeToNextDay);
-    } else {
-      // index does NOT divide by 7 evenly, add 1 hour to the previous dateTime
-      // CHANGE: every 4 hours 9:30, then 12:30 then 4:30
-      prevDateTime = new Date(prev.dateTime);
-      var timeToNextPoint = 4 * msInHour;
-      dateTime = new Date(prevDateTime.getTime() + timeToNextPoint);
+      if (prevDateTime.getDay() === 4 && index % 15 === 0) {
+        // skip weekends, so that every 5 days, with three points per day, skip to monday
+        const timeToNextMonday = (16 + 48) * msInHour;
+        dateTime = new Date(prevDateTime.getTime() + timeToNextMonday);
+      } else if (index % 3 === 0) {
+        // index DOES divide by 7 evenly, add the amount of milliseconds to the next day's 9:30 AM
+        // CHANGE: checking how 1 day looks being rendered by 3 points since with too
+        // many points per day they don't fit in the EC2 instance
+        // from 4:30 pm to 9:30 am, there are 17 hours
+        const timeToNextDay = 16 * msInHour;
+        dateTime = new Date(prevDateTime.getTime() + timeToNextDay);
+      } else {
+        // index does NOT divide by 7 evenly, add 1 hour to the previous dateTime
+        // CHANGE: every 4 hours 9:30, then 12:30 then 4:30
+        const timeToNextPoint = 4 * msInHour;
+        dateTime = new Date(prevDateTime.getTime() + timeToNextPoint);
+      }
     }
-
     return Object.assign({}, priceObject, { dateTime: dateTime.toISOString() });
   },
 
