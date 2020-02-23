76591781b44ad5d05f4ece3e83dc676d78ef286f
Manuela Gomez <mmanu.gomez@gmail.com>
2020-02-23 18:42:21 -0500

    Cleaned up comments and use of dotenv

--- seeds/influx/index.js
+++ seeds/influx/index.js
@@ -1,12 +1,10 @@
 const { seedInfluxDB } = require('./influx-seed.js');
 const config = require('../env.config.js');
-const dotenv = require('dotenv');
-dotenv.config();
-// What changes is time are the prices:
+require('dotenv').config();
+// What changes in time are the prices:
 const measurement = 'prices';
 // How many unique records or tickers
 const records = 1350000; // 18329343.695ms = 5 hours
-// should the orgID go into .env?
 const connection = {
   hostname: config.SERVICE_CHART_URL,
   token: process.env.INFLUX_TOKEN,
@@ -14,6 +12,5 @@ const connection = {
   orgID: process.env.ORG_ID
   precision: 'ms'
 }
-
 // seed database
 seedInfluxDB(measurement, records, connection);
