5cc0709249c45d2b79db543215868dcc149073eb
Manuela Gomez <mmanu.gomez@gmail.com>
2020-02-23 17:31:06 -0500

    [WIP] using environment variables and config to establish connection to DB

--- seeds/influx/index.js
+++ seeds/influx/index.js
@@ -1,20 +1,17 @@
 const { seedInfluxDB } = require('./influx-seed.js');
-// these need to go into the .env if choosing this database
-const influxURL = 'http://localhost';
-const token = process.env.INFLUXDB;
-
+const config = require('../env.config.js');
+const dotenv = require('dotenv');
+dotenv.config();
 // What changes is time are the prices:
 const measurement = 'prices';
 // How many unique records or tickers
 const records = 1350000; // 18329343.695ms = 5 hours
-const bucket = 'robinhood-chart';
 // should the orgID go into .env?
-const orgID = '66c5cd13f69410bd';
 const connection = {
-  hostname: influxURL,
-  token: token,
-  bucket: bucket,
-  orgID: orgID,
+  hostname: config.SERVICE_CHART_URL,
+  token: process.env.INFLUX_TOKEN,
+  bucket: config.BUCKET,
+  orgID: process.env.ORG_ID
   precision: 'ms'
 }
 
