69af6ba41821d305ea35ae65da168a0ac05dcf13
Manuela Gomez <mmanu.gomez@gmail.com>
2020-02-23 20:49:53 -0500

    [WIP] Integration tests for new database

--- tests/endpoints.test.js
+++ tests/endpoints.test.js
@@ -16,35 +16,85 @@ const seedFactory = () => {
   return tickerList;
 };
 
-// ENDPOINT TESTS
-describe('GET /price/:ticker', () => {
+describe('Endpoint tests', () => {
   const tickerList = seedFactory();
   // get a random ticker from the list
   const tickerObj = tickerList[Math.floor(Math.random() * tickerList.length)]
-  const url = `http://localhost:${PORT}/price/${tickerObj.ticker}`;
-
-  // check that the response is equal to the original ticker
-  it(`Should return a JSON object of that ticker's price data`, (done) => {
-    // call the endpoint with that ticker
-    request({ url, json: true }, (err, response, body) => {
-      if (err) { return console.log(err); }
-      expect(body.ticker).toEqual(tickerObj.ticker);
-      expect(body.name).toEqual(expect.any(String));
-      expect(body.prices[Math.floor(Math.random() * 390)].volume).toEqual(expect.any(Number));
-      expect(body.prices).toEqual(expect.any(Array));
-      done();
+  describe('GET /price/:ticker', () => {
+    const url = `http://localhost:${PORT}/price/${tickerObj.ticker}`;
+    // check that the response is equal to the original ticker
+    it(`Should return a JSON object of that ticker's price data`, (done) => {
+      // call the endpoint with that ticker
+      request({ url, json: true }, (err, response, body) => {
+        if (err) { return console.log(err); }
+        expect(body.ticker).toEqual(tickerObj.ticker);
+        expect(body.name).toEqual(expect.any(String));
+        expect(body.prices[Math.floor(Math.random() * 390)].volume).toEqual(expect.any(Number));
+        expect(body.prices).toEqual(expect.any(Array));
+        done();
+      });
+    });
+    //
+    it(`Should return a 500 error for a ticker that is not in the database`, (done) => {
+      // call the endpoint with that ticker
+      const badTickerURL = `http://localhost:${PORT}/price/NOTINDBS`;
+      request({ url: badTickerURL, json: true }, (err, response, body) => {
+        if (err) {
+          return console.log(err)
+        }
+        expect(response.statusCode).toEqual(500);
+        done();
+      });
     });
   });
-  //
-  it(`Should return a 500 error for a ticker that is not in the database`, (done) => {
-    // call the endpoint with that ticker
-    const badTickerURL = `http://localhost:${PORT}/price/NOTINDBS`;
-    request({ url: badTickerURL, json: true }, (err, response, body) => {
-      if (err) {
-        return console.log(err)
-      }
-      expect(response.statusCode).toEqual(500);
-      done();
+  describe('GET /current-price/:ticker', () => {
+    // get a random ticker from the list
+    const currentPriceURL = `http://localhost:${PORT}/current-price/${tickerObj.ticker}`;
+    // check that the response is equal to the original ticker
+    it(`Should return a JSON object of that ticker's last closing price`, (done) => {
+      // call the endpoint with that ticker
+      request({ url: currentPriceURL, json: true }, (err, response, body) => {
+        if (err) { return console.log(err); }
+        expect(body.currentPrice).toEqual(expect.any(Number));
+        done();
+      });
+    });
+    //
+    it(`Should return a 500 error for a ticker that is not in the database`, (done) => {
+      // call the endpoint with that ticker
+      const badTickerURL = `http://localhost:${PORT}/current-price/NOTINDBS`;
+      request({ url: badTickerURL, json: true }, (err, response, body) => {
+        if (err) {
+          return console.log(err)
+        }
+        expect(response.statusCode).toEqual(500);
+        done();
+      });
+    });
+  });
+  describe('GET /percent-change/:ticker', () => {
+    // get a random ticker from the list
+    const changeURL = `http://localhost:${PORT}/percent-change/${tickerObj.ticker}`;
+    // check that the response is equal to the original ticker
+    it(`Should return a JSON object of that ticker's percent change between the last available price and the previous price`, (done) => {
+      // call the endpoint with that ticker
+      request({ url: changeURL, json: true }, (err, response, body) => {
+        if (err) { return console.log(err); }
+        expect(body.percentChange).toEqual(expect.any(String));
+        done();
+      });
+    });
+    //
+    it(`Should return a 500 error for a ticker that is not in the database`, (done) => {
+      // call the endpoint with that ticker
+      const badTickerURL = `http://localhost:${PORT}/percent-change/NOTINDBS`;
+      request({ url: badTickerURL, json: true }, (err, response, body) => {
+        if (err) {
+          return console.log(err)
+        }
+        expect(response.statusCode).toEqual(500);
+        done();
+      });
     });
   });
 });
