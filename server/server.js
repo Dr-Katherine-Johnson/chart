b1ce8cb1100c5342b1930fab625330a0947330f3
Manuela Gomez <mmanu.gomez@gmail.com>
2020-03-04 01:09:04 -0500

    [WIP] added bodyParser and Post route

--- server/server.js
+++ server/server.js
@@ -5,12 +5,16 @@ const config = require('../env.config.js');
 const PORT = config.SERVICE_CHART_PORT;
 const controller = require('../controller/index.js');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('client/dist'));
 app.use(express.static('client/public'));
 
 app.get('/price/:ticker', controller.getTicker);
+app.post('/price/:ticker', controller.updateTicker);
 app.get('/current-price/:ticker', controller.getCurrentPrice);
 app.get('/percent-change/:ticker', controller.getPercentChange);
 
