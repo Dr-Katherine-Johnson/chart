cecf8f95881b6fb2b1591341049fad3166aa243f
Manuela Gomez <mmanu.gomez@gmail.com>
2020-03-04 20:46:22 -0500

    Refactored tests to use chai syntax; all tests passing

--- tests/endpoints.test.js
+++ tests/endpoints.test.js
@@ -22,83 +22,86 @@ const seedFactory = () => {
 describe('Endpoint tests', () => {
   const tickerList = seedFactory();
   // get a random ticker from the list
-  const tickerObj = tickerList[Math.floor(Math.random() * tickerList.length)]
+  const tickerObj = tickerList[Math.floor(Math.random() * tickerList.length)];
+  const badTicker = `NOTINDBS`;
   describe('GET /price/:ticker', () => {
-    const url = `http://localhost:${PORT}/price/${tickerObj.ticker}`;
+    const priceURL = `http://localhost:${PORT}/price/`;
     // check that the response is equal to the original ticker
     it(`Should return a JSON object of that ticker's price data`, (done) => {
       // call the endpoint with that ticker
-      // TODO Refactor to chai syntax
-      request({ url, json: true }, (err, response, body) => {
-        if (err) { return console.log(err); }
-        expect(body.ticker).toEqual(tickerObj.ticker);
-        expect(body.name).toEqual(expect.any(String));
-        expect(body.prices[Math.floor(Math.random() * 390)].volume).toEqual(expect.any(Number));
-        expect(body.prices).toEqual(expect.any(Array));
-        done();
-      });
+      // TODO check how the chai syntax checks for "any(String)"
+      chai.request(priceURL)
+        .get(tickerObj.ticker)
+        .end((err, res) => {
+          expect(err).to.be.null;
+          const body = res.body;
+          expect(body.ticker).to.equal(tickerObj.ticker);
+          expect(typeof body.name).to.equal('string');
+          expect(body.prices).to.be.a('array');
+          const randomPrice = body.prices[Math.floor(Math.random()*390)];
+          expect(randomPrice).to.have.all.keys('low','high','open','close','volume','dateTime');
+          done()
+        })
     });
     //
     it(`Should return a 500 error for a ticker that is not in the database`, (done) => {
-      // call the endpoint with that ticker
-      const badTickerURL = `http://localhost:${PORT}/price/NOTINDBS`;
-      request({ url: badTickerURL, json: true }, (err, response, body) => {
-        if (err) {
-          return console.log(err)
-        }
-        expect(response.statusCode).toEqual(500);
-        done();
-      });
+      // call the endpoint with a bad ticker
+      chai.request(priceURL)
+        .get(badTicker)
+        .end((err, res) => {
+          expect(res).to.have.status(500);
+          done();
+        })
     });
   });
   describe('GET /current-price/:ticker', () => {
     // get a random ticker from the list
-    const currentPriceURL = `http://localhost:${PORT}/current-price/${tickerObj.ticker}`;
+    const currentPriceURL = `http://localhost:${PORT}/current-price/`;
     // check that the response is equal to the original ticker
     it(`Should return a JSON object of that ticker's last closing price`, (done) => {
       // call the endpoint with that ticker
-      request({ url: currentPriceURL, json: true }, (err, response, body) => {
-        if (err) { return console.log(err); }
-        expect(body.currentPrice).toEqual(expect.any(Number));
-        done();
-      });
+      chai.request(currentPriceURL)
+        .get(tickerObj.ticker)
+        .end((err, res) => {
+          const body = res.body;
+          expect(body.currentPrice).to.be.a('number');
+          done();
+        })
     });
     //
     it(`Should return a 500 error for a ticker that is not in the database`, (done) => {
       // call the endpoint with that ticker
-      const badTickerURL = `http://localhost:${PORT}/current-price/NOTINDBS`;
-      request({ url: badTickerURL, json: true }, (err, response, body) => {
-        if (err) {
-          return console.log(err)
-        }
-        expect(response.statusCode).toEqual(500);
-        done();
-      });
+      chai.request(currentPriceURL)
+        .get(badTicker)
+        .end((err, res) => {
+          expect(res).to.have.status(500);
+          done();
+        })
     });
   });
   describe('GET /percent-change/:ticker', () => {
     // get a random ticker from the list
-    const changeURL = `http://localhost:${PORT}/percent-change/${tickerObj.ticker}`;
+    const changeURL = `http://localhost:${PORT}/percent-change/`;
     // check that the response is equal to the original ticker
     it(`Should return a JSON object of that ticker's percent change between the last available price and the previous price`, (done) => {
       // call the endpoint with that ticker
-      request({ url: changeURL, json: true }, (err, response, body) => {
-        if (err) { return console.log(err); }
-        expect(body.percentChange).toEqual(expect.any(String));
+      chai.request(changeURL)
+      .get(tickerObj.ticker)
+      .end((err, res) => {
+        const body = res.body;
+        expect(body.percentChange).to.be.a('string');
         done();
-      });
+      })
     });
     //
     it(`Should return a 500 error for a ticker that is not in the database`, (done) => {
       // call the endpoint with that ticker
-      const badTickerURL = `http://localhost:${PORT}/percent-change/NOTINDBS`;
-      request({ url: badTickerURL, json: true }, (err, response, body) => {
-        if (err) {
-          return console.log(err)
-        }
-        expect(response.statusCode).toEqual(500);
-        done();
-      });
+      chai.request(changeURL)
+        .get(badTicker)
+        .end((err, res) => {
+          expect(res).to.have.status(500);
+          done();
+        })
     });
   });
   describe('POST /price/:ticker', () => {
@@ -126,8 +129,7 @@ describe('Endpoint tests', () => {
           expect(err).to.be.null;
           expect(res).to.have.status(201);
           done()
-         }
-        )
+         })
     });
 
     it(`Should return a 500 error for a ticker that is not in the database`, (done) => {
