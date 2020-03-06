cf35a6e76899d5bb9471e5d5a994f15fa2bbb331
Manuela Gomez <mmanu.gomez@gmail.com>
2020-03-05 23:09:03 -0500

    [WIP] added middleware for testing with numbers instead of ticker strings

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const config = require('../env.config.js');
 const PORT = config.SERVICE_CHART_PORT;
+const middleware = require('./middleware.js');
 const controller = require('../controller/index.js');
 const cors = require('cors');
 const bodyParser = require('body-parser');
@@ -12,8 +13,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('client/dist'));
 app.use(express.static('client/public'));
-
-app.get('/price/:ticker', controller.getTicker);
+// in order to test more easily we use some middleware to check
+// if the ticker is given as a number or ticker
+// but in order to have a pseudo-chache of the array of tickers
+// ideally, we'd want to only do so if we're testing, not in production or dev
+middleware.generateTickers();
+app.get('/price/:ticker', middleware.convertIDtoTicker, controller.getTicker);
+// you can put the middleware in the functions you will be using for testing
 app.put('/price/:ticker', controller.updateTicker);
 app.get('/current-price/:ticker', controller.getCurrentPrice);
 app.get('/percent-change/:ticker', controller.getPercentChange);
