1997624d1683625c3a7e6e1d735289649fa8c323
Manuela Gomez <mmanu.gomez@gmail.com>
2020-03-07 09:40:37 -0500

    Testing last 10% of records

--- k6-stress-testing.js
+++ k6-stress-testing.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 
 // we can test in terms of rps by limiting how many requests each VU is able to make per second
-let desiredRPS = 400; //total rps for the test
-let RPSperVU = 22; // max requests exec by one VU per second
+let desiredRPS = 3000; //total rps for the test
+let RPSperVU = 180; // max requests exec by one VU per second
 // we do a unit conversion to get VUs required
 // desired RPS
 // ---------- => VU = desiredRPs/RPSperVU
@@ -16,13 +16,13 @@ export const options = {
   vus: VUsRequired,
   duration: '30s',
 };
-
-// is there a way to generate a random ticker to ask from the database?
-// for get requests
-const ticker = 'ABCDE';
+// we want to test against the last 10% of the tickers
+const max = 1350000 - 1;
+const last10percent = Math.floor(max * .1);
 
 export default function() {
   let iterationStart = new Date().getTime();
+  const ticker = Math.floor(Math.random()*last10percent);
   for (let i of Array(RPSperVU).keys()) {
     const url = `http://localhost:4444/price/${ticker}`;
     // get route
@@ -43,7 +43,6 @@ export default function() {
       },
     };
     const res = http.post(url, payload, params);
-    // post next
     let iterationDuration = (new Date().getTime() - iterationStart)/1000;
     // since requests are every second we can check if the virtual user needs
     // to sleep between executions
