9dceff04aa151608f6e5b436d1a4f563d5176192
Manuela Gomez <mmanu.gomez@gmail.com>
2020-03-12 21:39:53 -0400

    Added memcached url

--- server/env.config.js
+++ server/env.config.js
@@ -2,6 +2,7 @@ let obj = {
   BUCKET: 'robinhood-chart',
   SERVICE_CHART_PORT: `4444`,
   SERVICE_CHART_URL: process.env.SERVICE_CHART_URL,
+  MEMCACHED_URL: `memcached://localhost:11211`,
 };
 // console.log(obj);
 module.exports = obj;
